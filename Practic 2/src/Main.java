d0ab346d15d02daf45929d08876b41820bb93e6a
Leshka <31736716+TOPlessss@users.noreply.github.com>
2018-10-09 22:06:07 +0300

    Add files via upload

---
+++ Practic 2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Author au = new Author("Paul", "paul_the_author@mail.ru", 'M');
+        System.out.println(au.toString());
+    }
+}
